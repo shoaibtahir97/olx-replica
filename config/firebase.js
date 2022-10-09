676413ae37ecc24eac5055bfc48d20f8b2d4c923
Muhammad Shoaib Tahir <shoaibtahir97@gmail.com>
2022-10-09 12:21:54 +0500

    made some changes in index file and js file to display fullname on sign up and store the data to firebase

--- config/firebase.js
+++ config/firebase.js
@@ -2,6 +2,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp,  } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
 import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js'
+import {getFirestore, collection, addDoc} from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,13 +20,18 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-function signUpNewUser(email,password){
+// Get Firestore and store in variable
+const db = getFirestore(app);
+
+function signUpNewUser(userInfo){
+    const {email, password} = userInfo;
     createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
     // Signed in 
     const user = userCredential.user;
     // ...
     alert("Successfully registered ")
+    addUserToDB(userInfo)
   })
   .catch((error) => {
     const errorCode = error.code;
@@ -34,6 +40,14 @@ function signUpNewUser(email,password){
     console.log("Error ---> ", errorMessage);
   });
 }
+
+
+function addUserToDB(userInfo){
+  const {email, fullname, age } = userInfo
+
+  return addDoc(collection (db, "users"),{email, fullname, age})
+}
+
 
 function signInUser(){
   signInWithEmailAndPassword(auth, email, password)
--- signUp&signIn.js
+++ signUp&signIn.js
@@ -3,9 +3,10 @@ import {signUpNewUser, signInUser} from './config/firebase.js';
 window.signUp = function (){
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
+    const fullname = document.getElementById("fullname").value;
+    const age = document.getElementById("age").value;
 
-
-    signUpNewUser(email, password)
+    signUpNewUser({email, password, fullname, age})
 
 }
 
